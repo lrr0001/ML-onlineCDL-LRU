9ae71794ad09924329f72a36ae3a8a19a150032d
LRR <lrichert3@gatech.edu>
2021-03-30 11:15:41 -0500

    Removed some memory leaks from Transforms.py.

--- transforms.py
+++ transforms.py
@@ -5,34 +5,38 @@ class fft2d(tf.keras.layers.Layer):
     def __init__(self,fftSz,*args,**kwargs):
         self.fftSz = fftSz
         super().__init__(*args,autocast=False,**kwargs)
-    def call(self,inputs):
+    def fft(self,inputs):
         return tf.signal.rfft2d(input_tensor=inputs,fft_length=self.fftSz)
+    def call(self,inputs):
+        return self.fft(inputs)
     def get_config(self):
         return {'fftSz': fftSz}
 
+def _sscf(inputs):
+    return tf.transpose(a=inputs,perm=(0,3,4,1,2),conjugate=False)
 
 class switch_spatial_and_channelfltr(tf.keras.layers.Layer):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,autocast=False,**kwargs)
     def call(self,inputs):
-        return tf.transpose(a=inputs,perm=(0,3,4,1,2),conjugate=False)
+        return _sscf(inputs)
 
 class ifft2d(fft2d):
-    def call(self,inputs):
+    def fft(self,inputs):
         return tf.signal.irfft2d(input_tensor=inputs,fft_length=self.fftSz)
 
 class fft2d_inner(fft2d):
     def call(self,inputs):
-        return switch_spatial_and_channelfltr()(fft2d(self.fftSz)(switch_spatial_and_channelfltr()(inputs)))
+        return _sscf(self.fft(_sscf(inputs)))
 
-class ifft2d_inner(fft2d):
+class ifft2d_inner(ifft2d):
     def call(self,inputs):
-        return switch_spatial_and_channelfltr()(ifft2d(self.fftSz)(switch_spatial_and_channelfltr()(inputs)))
+        return _sscf(self.fft(_sscf(inputs)))
 
 class fft2d_multichannel(fft2d):
     def call(self,inputs):
-        return util.rotate_dims_left(fft2d(self.fftSz)(util.rotate_dims_right(inputs,4)),4)
+        return util.rotate_dims_left(self.fft(util.rotate_dims_right(inputs,4)),4)
 
-class ifft2d_multichannel(fft2d):
+class ifft2d_multichannel(ifft2d):
     def call(self,inputs):
-        return util.rotate_dims_left(ifft2d(self.fftSz)(util.rotate_dims_right(inputs,4)),4)
+        return util.rotate_dims_left(self.fft(util.rotate_dims_right(inputs,4)),4)
