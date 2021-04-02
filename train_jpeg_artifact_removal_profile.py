e552bff0093b7442de2fd8a373e74a6fd5b3b05a
LRR <lrichert3@gatech.edu>
2021-04-02 17:37:05 -0500

    set histogram to 0. I just want the computational graphs.

--- train_jpeg_artifact_removal_profile.py
+++ train_jpeg_artifact_removal_profile.py
@@ -132,7 +132,7 @@ class TimeHistory(tf.keras.callbacks.Callback):
 log_dir = "logs/logs_test/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S.log")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(
       log_dir = log_dir + 'no_profiler.log',
-      histogram_freq = 1#,
+      histogram_freq = 0#,
 #      profile_batch = '2,5'
 )
 
