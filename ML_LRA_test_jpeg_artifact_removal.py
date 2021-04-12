Fixed the quantization handling, and alpha now backpropagates properly.

ML_LRA_test_jpeg_artifact_removal.py
@@ -11,17 +11,17 @@ rho = 1.
 alpha_init = 1.5
 mu_init = 1.
 b_init = 0.
-lraParam = {'n_components': 4}
+n_components = 4
 cmplxdtype = tf.complex128 # This should really be elsewhere.
-batch_size = 4
-steps_per_epoch = 4
-step_size = 0.1
-num_of_epochs = 3
+batch_size = 1
+steps_per_epoch = 3150
+step_size = 0.01
+num_of_epochs = 8
 
 
 #   ******** DATA AND EXPERIMENT PARAMETERS ********
-modelname = 'ML_LRA_'
-databasename = 'simpleTest/'
+modelname = 'ML_LRA_SGD'
+databasename = 'BSDS500/'
 experimentname = 'experiment1/'
 experimentpath = 'data/experiment/' + databasename + experimentname
 checkpointfilename = modelname + 'checkpoint_epoch_{epoch:02d}.ckpt'
@@ -42,6 +42,7 @@ noL = problem_param['noL']
 noc = problem_param['noc']
 datapath = problem_param['datapath']
 trainfile = problem_param['trainfile']
+valfile = problem_param['valfile']
 padding = data_param['padding']
 
 
@@ -72,7 +73,7 @@ def _parse_image_function(example_proto):
     lowpass = restore_double(x['lowpass'])
     return ((highpass[slice(startr,endr),slice(startc,endc),slice(None)],lowpass[slice(startr,endr),slice(startc,endc),slice(None)],restore_double(x['compressed'])),restore_double(x['raw']))
 
-raw_dataset = tf.data.TFRecordDataset([datapath + trainfile])
+raw_dataset = tf.data.TFRecordDataset([datapath + valfile])
 dataset = raw_dataset.map(_parse_image_function)
 dataset_batch = dataset.batch(batch_size)
 
@@ -94,7 +95,7 @@ for (x,y) in dataset_batch:
     break
 
 #   ******** BUILD MODEL ********
-CSC = mlcsc.MultiLayerCSC(rho,alpha_init,mu_init,b_init,qY,qUV,cropAndMerge,fftSz,strides,problem_param['D'],lraParam,noi,noL,cmplxdtype)
+CSC = mlcsc.MultiLayerCSC(rho,alpha_init,mu_init,b_init,qY,qUV,cropAndMerge,fftSz,strides,problem_param['D'],n_components,noi,noL,cmplxdtype)
 
 # Build Input Layers
 highpassShape = (targetSz[0] + paddingTuple[0][0] + paddingTuple[0][1],targetSz[1] + paddingTuple[1][0] + paddingTuple[1][1],noc)
@@ -107,7 +108,35 @@ reconstruction,itstats = CSC(inputs)
 rgb_reconstruction = jrf.YUV2RGB(dtype=real_dtype)(reconstruction)
 clipped_reconstruction = util.clip(a = 0.,b = 1.,dtype=real_dtype)(rgb_reconstruction)
 import post_process_grad as ppg
-model = ppg.Model_PostProcess(inputs,clipped_reconstruction)
+#model = ppg.Model_PostProcess(inputs,clipped_reconstruction)
+model = tf.keras.Model(inputs,clipped_reconstruction)
+
+
+#   ******** COMPILE AND TRAIN MODEL ********
+import time
+class TimeHistory(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.train_times = []
+
+    def on_test_begin(self, logs={}):
+        self.test_times = []
+
+    def on_test_batch_begin(self, batch, logs={}):
+        self.test_batch_start_time = time.time()
+
+    def on_test_batch_end(self, batch,logs={}):
+        self.test_times.append(time.time() - self.test_batch_start_time)
+
+    def on_epoch_begin(self, batch, logs={}):
+        self.epoch_time_start = time.time()
+
+    def on_epoch_end(self, batch, logs={}):
+        self.train_times.append(time.time() - self.epoch_time_start)
+
+
+model.compile(optimizer = tf.keras.optimizers.SGD(step_size),loss = tf.keras.losses.MSE,run_eagerly=False)
+for tv in model.trainable_variables:
+    print(tv.name)
 
 #   ******** COMPILE AND TEST MODEL ********
 mse = tf.keras.metrics.MeanSquaredError(dtype=real_dtype)

ML_LRA_train_jpeg_artifact_removal.py
@@ -14,14 +14,14 @@ b_init = 0.
 n_components = 4
 cmplxdtype = tf.complex128 # This should really be elsewhere.
 batch_size = 1
-steps_per_epoch = 3150
+steps_per_epoch = 2
 step_size = 0.01
-num_of_epochs = 8
+num_of_epochs = 2
 
 
 #   ******** DATA AND EXPERIMENT PARAMETERS ********
 modelname = 'ML_LRA_SGD'
-databasename = 'BSDS500/'
+databasename = 'simpleTest/'
 experimentname = 'experiment1/'
 experimentpath = 'data/experiment/' + databasename + experimentname
 checkpointfilename = modelname + 'checkpoint_epoch_{epoch:02d}.ckpt'

generate_low_freq_imgs.py
@@ -10,7 +10,7 @@ noi = 20
 lmbda = 0.1
 dtype = 'float64'
 
-datasetname = 'SimpleTest/'
+datasetname = 'simpleTest/'
 
 # Obtain quantization matrices from chosen quality factor
 import PIL
@@ -38,7 +38,8 @@ Yoffset = tf.one_hot([[[0]]],64,tf.cast(32.,dtype = dtype),tf.cast(0.,dtype= dty
 dataPath = 'data/original/' + datasetname
 filelist = os.listdir(dataPath)
 savePath = 'data/scratchwork/' + datasetname + '/whole/'
-for datatype in ['train/','val/','test/']:
+#for datatype in ['train/','val/','test/']:
+for datatype in ['train/','val/']:
     filelist = os.listdir(dataPath + datatype)
     for filename in filelist:
         loadedImg = PIL.Image.open(dataPath + datatype + filename)

jpeg_related_functions.py
@@ -29,7 +29,7 @@ class Smooth_JPEG(optmz.ADMM):
         return (None,None)
     def init_y(self,s,x,Ax,negC):
         #return (self.Wt(negC),negC)
-        self.Wt(negC)
+        return (self.Wt(negC),None)
     def init_u(self,s,Ax,By,negC):
         #return [0.,0.,0.]
         return (None,)
@@ -50,7 +50,7 @@ class Smooth_JPEG(optmz.ADMM):
     def ystep(self,x,u,Ax_relaxed,negC):
         return (self.yupdate((x,negC)),None)
     def ustep(self,u,Ax_relaxed,By,negC):
-        return self.init_u((u,Ax_relaxed,By,negC))
+        return (None,)
 
     # Before and After:
     def preprocess(self,s):
@@ -369,21 +369,20 @@ class QuantizationAdjustment(tf.keras.layers.Layer):
 class Enforce_JPEG_Constraint(tf.keras.layers.Layer):
     def __init__(self,qY,qUV,W,Wt,*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.Yoffset = tf.one_hot([[[0]]],64,tf.cast(32.,self.dtype),tf.cast(0.,self.dtype))
         self.qY = qY
         self.qUV = qUV
         self.W = W
         self.Wt = Wt
     def get_config(self):
-        return {'Yoffset': self.Yoffset, 'qY': self.qY, 'qUV': self.qUV}
+        return {'qY': self.qY, 'qUV': self.qUV}
     def call(self,inputs):
         fx,negC = inputs
         zero = tf.cast(0.,self.dtype)
-        max_value = [negC[channel] + offset + q/2 for (channel,q,offset) in zip(range(negC),(qY,qUV,qUV),(Yoffset,zero,zero))]
-        min_value = [negC[channel] + offset - q/2 for (channel,q,offset) in zip(range(negC),(qY,qUV,qUV),(Yoffset,zero,zero)]
+        max_value = [negC[channel] + q/2. for (channel,q) in zip(range(len(negC)),(self.qY,self.qUV,self.qUV))]
+        min_value = [negC[channel] - q/2. for (channel,q) in zip(range(len(negC)),(self.qY,self.qUV,self.qUV))]
         Wx = self.W(fx)
-        delta_value = [tf.where(Wx[channel] + offset > max_value,max_value + offset - Wx[channel],zero) for (channel,offset) in zip(range(negC),(self.Yoffset,zero,zero))]
-        delta_value = [tf.where(Wz[channel] + offset < min_value,min_value + offset - Wx[channel],zero) for (channel,offset) in zip(range(negC),(self.Yoffset,zero,zero))]
+        delta_value = [tf.where(Wx[channel] > max_value[channel],max_value[channel] - Wx[channel],zero) for channel in range(len(negC))]
+        delta_value = [tf.where(Wx[channel] < min_value[channel],min_value[channel] - Wx[channel],zero) for channel in range(len(negC))]
         return self.Wt(delta_value)
 
 class ZUpdate_JPEG_Implicit(tf.keras.layers.Layer):
@@ -392,6 +391,6 @@ class ZUpdate_JPEG_Implicit(tf.keras.layers.Layer):
         self.enforce_jpeg_constraint = Enforce_JPEG_Constraint(qY,qUV,W,Wt,*args,**kwargs)
     def call(self,inputs):
         fx,negC = inputs
-        delta_z = self.enforce_jpeg_constraint(fx,negC)
+        delta_z = self.enforce_jpeg_constraint((fx,negC))
         z = fx + delta_z
-        return (z,z)
+        return z

optmz.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 class ADMM(tf.keras.layers.Layer):
     def __init__(self,rho,alpha,noi,*args,**kwargs):
         self.rho = rho
-        self.alpha = alpha
+#        self.alpha = alpha
         self.noi = noi
         super().__init__(*args,**kwargs)
 
@@ -44,6 +44,66 @@ class ADMM(tf.keras.layers.Layer):
         y,By = self.ystep(x,u,Ax_relaxed,negC)
         u = self.ustep(u,Ax_relaxed,By,negC)
         itstats = self.itstats_record(x,y,u,Ax,Ax_relaxed,By,negC,itstats)
+        return (y,u,By,itstats)
+
+    # Before and After:
+    def preprocess(self,s):
+        return s
+    def get_output(self,s,y,u,By,negC,itstats):
+        x,Ax = self.xstep(y,u,By,negC)
+        return (x,itstats)
+
+    # The Call function    
+    def call(self,s):
+        s = self.preprocess(s)
+        y,u,By,negC,itstats = self.init_vars(s)
+        for ii in range(self.noi):
+            y,u,By,itstats = self.solvestep(y,u,By,negC,itstats)
+        return self.get_output(s,y,u,By,negC,itstats)
+
+class ADMM_Relaxed(tf.keras.layers.Layer):
+    def __init__(self,rho,alpha,noi,*args,**kwargs):
+        self.rho = rho
+        self.noi = noi
+        super().__init__(*args,**kwargs)
+
+    # These initializations happen once per input (negC,y,By,u):
+    def init_vars(self,s):
+        negC = self.get_negative_C(s)
+        x,Ax = self.init_x(s,negC)
+        y,By = self.init_y(s,x,Ax,negC)
+        u = self.init_u(s,Ax,By,negC)
+        itstats = self.init_itstats(s)
+        return (y,u,By,negC,itstats)
+    def init_x(self,s,negC):
+        raise NotImplementedError
+    def init_y(self,s,x,Ax,negC):
+        raise NotImplementedError
+    def init_u(self,s,Ax,By,negC):
+        raise NotImplementedError
+    def get_negative_C(self,s):
+        raise NotImplementedError
+    def init_itstats(self,s):
+        return []
+
+
+    # iterative steps:
+    def xstep(self,y,u,By,negC):
+        raise NotImplementedError
+    def relax(self,u,Ax,By,negC):
+        raise NotImplementedError
+    def ystep(self,x,u,Ax,negC):
+        raise NotImplementedError
+    def ustep(self,u,Ax,By,negC):
+        raise NotImplementedError
+    def itstats_record(self,x,y,u,Ax,By,negC,itstats):
+        return itstats
+    def solvestep(self,y,u,By,negC,itstats):
+        x,Ax = self.xstep(y,u,By,negC)
+        u = self.relax(u,Ax,By,negC)
+        y,By = self.ystep(x,u,Ax,negC)
+        u = self.ustep(u,Ax,By,negC)
+        itstats = self.itstats_record(x,y,u,Ax,By,negC,itstats)
         return (y,u,By,itstats)
 
     # Before and After:

util.py
@@ -149,10 +149,10 @@ class BiasedReLU(tf.keras.layers.Layer):
         x,bias = inputs
         return self.relu(x - bias)
 
-class Shrinkage(tr.keras.layers.Layer):
+class Shrinkage(tf.keras.layers.Layer):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.relu = BiasedReLU(*args,**kwargs)
     def call(self,inputs):
         x,bias = inputs
-        return self.relu(x,bias) - self.relu(-x,bias)
+        return self.relu((x,bias)) - self.relu((-x,bias))