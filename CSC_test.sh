2b5f0365c2bf8265e0b9d52dad3859523a14ab59
LRR <lrichert3@gatech.edu>
2021-09-08 08:05:14 -0400

    comment out admm so it doesn't try to compute those again.

--- CSC_test.sh
+++ CSC_test.sh
@@ -2,10 +2,10 @@
 databasename='BSDS500/'
 for ii in {0..48..4}
   do
-    for logrho in {-6..9..3}
-      do
-        python CSCadmmScript.py $databasename $logrho $ii
-      done
+#    for logrho in {-6..9..3}
+#      do
+#        python CSCadmmScript.py $databasename $logrho $ii
+#      done
     for loglpstz in {0..15..3}
       do
         python CSCfistaScript.py $databasename $loglpstz $ii
