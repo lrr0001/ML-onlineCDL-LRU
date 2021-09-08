a936c8110264097e6f6f320ed4e82bcf4c1d9e79
LRR <lrichert3@gatech.edu>
2021-09-08 08:03:14 -0400

    oops, overwrote lpstz.

--- CSCfistaScript.py
+++ CSCfistaScript.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     databasename = sys.argv[1]
     lpstz = 2**int(sys.argv[2])
     noi = int(sys.argv[3])
-    lpstz = 100
+    #lpstz = 100
     spe = 5
     noe = 13
     fista.test_FISTA_CSC(lpstz = lpstz,noi = noi,databasename = databasename,steps_per_epoch = spe, num_of_epochs = noe)
