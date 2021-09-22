18b3538652faf3b6f92e22fb2502ae504d615e05
LRR <lrichert3@gatech.edu>
2021-09-22 15:25:28 -0400

    Let's get to the bottom of this. Does FISTA (with few iterations) really perform better at JPEG artifact removal, even as it performs worse at convolutional sparse coding? Time to put it to the test!

---
+++ CSCfistaScript_learned_dict.py
@@ -0,0 +1,12 @@
+import ML_FISTA_eval_jpeg_artifact_removal as fista
+import sys
+
+if __name__ == '__main__':
+    databasename = sys.argv[1]
+    lpstz = 2**(int(sys.argv[2])/2)
+    noi = int(sys.argv[3])
+    pklfile = sys.argv[4]
+    #lpstz = 100
+    spe = 16
+    noe = 128
+    fista.test_FISTA_CSC_saved_dict(lpstz = lpstz,noi = noi,databasename = databasename,steps_per_epoch = spe, num_of_epochs = noe,pklfile = pklfile)
