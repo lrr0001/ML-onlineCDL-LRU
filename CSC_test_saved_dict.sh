f32608db38e7f3335fd9946e035d6de4df93012a
LRR <lrichert3@gatech.edu>
2021-10-25 00:25:04 -0400

    Going to test whether discrepancy between the intermediate outputs during training and the evaluation results have something to do with which reconstruction approach is used. Only file modified is the shell script.

--- CSC_test_saved_dict.sh
+++ CSC_test_saved_dict.sh
@@ -5,13 +5,13 @@ modelname + 'checkpoint_epoch_{epoch:02d}.ckpt'
 modelname='ML_LRA_'
 checkpointstr='checkpoint_epoch_0'
 extension='.ckpt.pkl'
-for ii in {1..9..1}
-  do
-    python CSCadmmScript_learned_dict.py $databasename 0 16 $modelname$checkpointstr$ii$extension
-  done
+#for ii in {1..9..1}
+#  do
+#    python CSCadmmScript_learned_dict.py $databasename 0 16 $modelname$checkpointstr$ii$extension
+#  done
 modelname='ML_FISTA_'
 extension='.pkl'
-for ii in {1..9..1}
+for ii in {98..99..1}
   do
     python CSCfistaScript_learned_dict.py $databasename 12 48 $modelname$checkpointstr$ii$extension
   done
