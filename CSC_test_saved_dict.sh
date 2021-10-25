1ecf03deabe9c0e9129ba0204388859fdc15c074
LRR <lrichert3@gatech.edu>
2021-10-25 15:53:30 -0400

    Adding code to specifically look at initial performance, so that my convergence curves can start at 0, rather than after some time has elapsed.

--- CSC_test_saved_dict.sh
+++ CSC_test_saved_dict.sh
@@ -15,17 +15,28 @@ databasename='BSDS500/'
 #  do
 #    python CSCfistaScript_learned_dict.py $databasename 12 48 $modelname$checkpointstr$ii$extension
 #  done
-modelname='ML_LRA_'
-checkpointstr='checkpoint_epoch_0'
-extension='.ckpt.pkl'
-for ii in {1..9..1}
-  do
-    python CSCadmmScript_learned_dict.py $databasename 0 16 $modelname$checkpointstr$ii$extension
-  done
+#modelname='ML_LRA_'
+#checkpointstr='checkpoint_epoch_0'
+#extension='.ckpt.pkl'
+#for ii in {1..9..1}
+#  do
+#    python CSCadmmScript_learned_dict.py $databasename 0 16 $modelname$checkpointstr$ii$extension
+#  done
 #modelname='ML_FISTA_'
 #extension='.pkl'
 #for ii in {1..9..1}
 #  do
 #    python CSCfistaScript_learned_dict.py $databasename 12 48 $modelname$checkpointstr$ii$extension
 #  done
+
+modelname='ML_LRA_'
+checkpointstr='checkpoint_epoch_0'
+extension='.ckpt.pkl'
+ii=0
+python CSCadmmScript_init.py $databasename 0 16 $modelname$checkpointstr$ii$extension
+
+modelname='ML_FISTA_'
+extension='.pkl'
+ii=0
+python CSCfistaScript_init.py $databasename 12 48 $modelname$checkpointstr$ii$extension
 
