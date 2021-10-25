a33c2f0f579b9ef4ee04abedcb6cf1d4fe93451a
LRR <lrichert3@gatech.edu>
2021-10-25 11:57:27 -0400

    Oops, didn't include modelname for the ADMM 1 through 9 run.

--- CSC_test_saved_dict.sh
+++ CSC_test_saved_dict.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 databasename='BSDS500/'
 
-modelname + 'checkpoint_epoch_{epoch:02d}.ckpt'
+#modelname + 'checkpoint_epoch_{epoch:02d}.ckpt'
+#modelname='ML_LRA_'
+#checkpointstr='checkpoint_epoch_'
+#extension='.ckpt.pkl'
+#for ii in {10..32..1}
+#  do
+#    python CSCadmmScript_learned_dict.py $databasename 0 16 $modelname$checkpointstr$ii$extension
+#  done
+#modelname='ML_FISTA_'
+#extension='.pkl'
+#for ii in {10..32..1}
+#  do
+#    python CSCfistaScript_learned_dict.py $databasename 12 48 $modelname$checkpointstr$ii$extension
+#  done
 modelname='ML_LRA_'
-checkpointstr='checkpoint_epoch_'
-extension='.ckpt.pkl'
-for ii in {10..32..1}
-  do
-    python CSCadmmScript_learned_dict.py $databasename 0 16 $modelname$checkpointstr$ii$extension
-  done
-modelname='ML_FISTA_'
-extension='.pkl'
-for ii in {10..32..1}
-  do
-    python CSCfistaScript_learned_dict.py $databasename 12 48 $modelname$checkpointstr$ii$extension
-  done
 checkpointstr='checkpoint_epoch_0'
 extension='.ckpt.pkl'
 for ii in {1..9..1}
   do
     python CSCadmmScript_learned_dict.py $databasename 0 16 $modelname$checkpointstr$ii$extension
   done
-modelname='ML_FISTA_'
-extension='.pkl'
-for ii in {1..9..1}
-  do
-    python CSCfistaScript_learned_dict.py $databasename 12 48 $modelname$checkpointstr$ii$extension
-  done
+#modelname='ML_FISTA_'
+#extension='.pkl'
+#for ii in {1..9..1}
+#  do
+#    python CSCfistaScript_learned_dict.py $databasename 12 48 $modelname$checkpointstr$ii$extension
+#  done
 
