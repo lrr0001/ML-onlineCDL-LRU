3316c17adce227f76407ae8b0f2dee1011c7edb7
LRR <lrichert3@gatech.edu>
2021-11-15 03:31:05 -0500

    Added python test script to check compressed image.

---
+++ check_cmprssd_img.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+import pickle as pkl
+fid = open('data/scratch/BSDS500/patches/train/compressed/271008.jpg.pckl_12_7.pckl','rb')
+cmprssdImg = pkl.load(fid)
+fid.close()
+
+print(tf.reduce_max(cmprssdImg))
+print(tf.reduce_min(cmprssdImg))
