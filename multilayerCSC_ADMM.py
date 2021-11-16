824503bae6ecbf125e973c4b91011b1482ca85aa
LRR <lrichert3@gatech.edu>
2021-11-16 03:05:47 -0500

    missed a tf.cond

--- multilayerCSC_ADMM.py
+++ multilayerCSC_ADMM.py
@@ -1061,7 +1061,7 @@ class GetNextIterZFreq_lastlayer(tf.keras.layers.Layer,ppg.PostProcess):
         b = tf.cond(update_cond,lambda: self.bprev + deltab,lambda: self.bprev)
         b = tf.where(b < 0.,tf.cast(0,dtype=tf.as_dtype(self.dtype).real_dtype),b)
         count = tf.cond(update_cond,lambda: 0,lambda: self.count + 1)
-        return [self.b.assign(b),self.count.assign(count),self.bprev.assign(b),self.deltab.assign(tf.cond(update_cond,tf.cast(0,dtype=tf.as_dtype(self.dtype).real_dtype),deltab))]
+        return [self.b.assign(b),self.count.assign(count),self.bprev.assign(b),self.deltab.assign(tf.cond(update_cond,lambda: tf.cast(0,dtype=tf.as_dtype(self.dtype).real_dtype),lambda: deltab))]
 
     def _update_mu(self):
         return [self.mu.assign(tf.where(self.mu < 1e-3,tf.cast(1e-3,dtype=tf.as_dtype(self.dtype).real_dtype),self.mu))]
